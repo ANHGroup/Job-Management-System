3e464305de3239b7c4f634c6efa574786b0aad23
tuhin cse <tuhincse20@gmail.com>
2023-04-13 14:35:38 +0600

    userwise data

--- app/Http/Controllers/AppliedJobController.php
+++ app/Http/Controllers/AppliedJobController.php
@@ -16,7 +16,13 @@ class AppliedJobController extends Controller
      */
     public function index()
     {
-
+        $user = Auth::user();
+        $jobs = DB::table('applied_jobs')
+            ->join('applicant_profiles', 'applicant_profiles.id', '=', 'applied_jobs.applicant_id')
+            ->join('jobs', 'jobs.id', '=', 'applied_jobs.job_id')
+            ->where('user_id', $user->id)
+            ->get();
+        return view('frontend.pages.appliedjobs.list', compact('jobs'));
     }
 
     /**
