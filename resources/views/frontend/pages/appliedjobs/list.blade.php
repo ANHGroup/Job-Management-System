ccec2bb3ae9289eec9857e24e83236bcea1a4512
tuhin cse <tuhincse20@gmail.com>
2023-04-16 14:31:55 +0600

    table center align

--- resources/views/frontend/pages/appliedjobs/list.blade.php
+++ resources/views/frontend/pages/appliedjobs/list.blade.php
@@ -1,35 +1,17 @@
 @extends('frontend.layouts.master')
 @section('content')
-    <div class="content-page">
+    <div class="container-fluid">
         <div class="content">
 
             <!-- Start Content-->
-            <div class="container-fluid">
-
-                <!-- start page title -->
-                <div class="row">
-                    <div class="col-12">
-                        <div class="page-title-box">
-
-                        </div>
-                    </div>
-                </div>
-                <!-- end page title -->
-
-
-                <div class="row">
+            <div class="">
+             <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
                                 <div class="row mb-2">
                                     View All Applied jobs: {{ Auth::user()->name }}
-                                    {{-- <div class="col-sm-8">
-                                    <div class="text-sm-right">
-                                        <button type="button" class="btn btn-success mb-2 mr-1"><i class="mdi mdi-settings"></i></button>
-                                        <button type="button" class="btn btn-secondary mb-2 mr-1">Import</button>
-                                        <button type="button" class="btn btn-secondary mb-2">Export</button>
-                                    </div>
-                                </div><!-- end col--> --}}
+                                   
                                 </div>
 
                                 <div class="table-responsive">
@@ -80,26 +62,7 @@
                                     </table>
                                 </div>
 
-                                <ul class="pagination pagination-rounded justify-content-end mb-0">
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Previous">
-                                            <span aria-hidden="true">«</span>
-                                            <span class="sr-only">Previous</span>
-                                        </a>
-                                    </li>
-                                    <li class="page-item active"><a class="page-link" href="javascript: void(0);">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">4</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">5</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Next">
-                                            <span aria-hidden="true">»</span>
-                                            <span class="sr-only">Next</span>
-                                        </a>
-                                    </li>
-                                </ul>
-
+                              
                             </div> <!-- end card-body-->
                         </div> <!-- end card-->
                     </div> <!-- end col -->
