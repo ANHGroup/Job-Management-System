d920a316713e85a4bab365e1c0ae77fa36957999
tuhin cse <tuhincse20@gmail.com>
2023-04-16 12:01:58 +0600

    code alignment

--- resources/views/frontend/pages/appliedjobs/list.blade.php
+++ resources/views/frontend/pages/appliedjobs/list.blade.php
@@ -15,7 +15,7 @@
                     </div>
                 </div>
                 <!-- end page title -->
-               <div class="row">
+                <div class="row">
                     <div class="col-12">
                         <div class="card">
                             <div class="card-body">
@@ -23,26 +23,14 @@
                                     <div class="col-sm-4">
                                         <h1> {{ Auth::user()->name }}</h1>
                                     </div>
-                                    {{-- <div class="col-sm-8">
-                                    <div class="text-sm-right">
-                                        <button type="button" class="btn btn-success mb-2 mr-1"><i class="mdi mdi-settings"></i></button>
-                                        <button type="button" class="btn btn-secondary mb-2 mr-1">Import</button>
-                                        <button type="button" class="btn btn-secondary mb-2">Export</button>
-                                    </div>
-                                </div><!-- end col--> --}}
+                                    
                                 </div>
 
                                 <div class="table-responsive">
                                     <table class="table table-centered table-striped" id="products-datatable">
                                         <thead>
                                             <tr>
-                                                {{-- <th style="width: 20px;">
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                                    <label class="custom-control-label" for="customCheck1">&nbsp;</label>
-                                                </div>
-                                            </th> --}}
-
+                                             
                                                 <th>Job Title</th>
                                                 <th>Expected Salary</th>
                                                 <th>Job Category</th>
@@ -74,30 +62,9 @@
                                                     </td>
                                                 </tr>
                                             @endforeach
-                                       </tbody>
+                                        </tbody>
                                     </table>
                                 </div>
-
-                                <ul class="pagination pagination-rounded justify-content-end mb-0">
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Previous">
-                                            <span aria-hidden="true">«</span>
-                                            <span class="sr-only">Previous</span>
-                                        </a>
-                                    </li>
-                                    <li class="page-item active"><a class="page-link" href="javascript: void(0);">1</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">2</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">3</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">4</a></li>
-                                    <li class="page-item"><a class="page-link" href="javascript: void(0);">5</a></li>
-                                    <li class="page-item">
-                                        <a class="page-link" href="javascript: void(0);" aria-label="Next">
-                                            <span aria-hidden="true">»</span>
-                                            <span class="sr-only">Next</span>
-                                        </a>
-                                    </li>
-                                </ul>
-
                             </div> <!-- end card-body-->
                         </div> <!-- end card-->
                     </div> <!-- end col -->
@@ -107,10 +74,5 @@
             </div> <!-- container -->
 
         </div> <!-- content -->
-
-        <!-- Footer Start -->
-
-        <!-- end Footer -->
-
-    </div>
+ </div>
 @endsection
