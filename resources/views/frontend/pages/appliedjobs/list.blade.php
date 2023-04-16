6140f1f83a2f16ebf89b15119fced21b8d8e2654
tuhin cse <tuhincse20@gmail.com>
2023-04-16 12:04:58 +0600

    removes file

--- resources/views/frontend/pages/appliedjobs/list.blade.php
+++ resources/views/frontend/pages/appliedjobs/list.blade.php
@@ -22,7 +22,7 @@
                         <div class="card">
                             <div class="card-body">
                                 <div class="row mb-2">
-                                   View All Applied jobs: {{ Auth::user()->name }}
+                                    View All Applied jobs: {{ Auth::user()->name }}
                                     {{-- <div class="col-sm-8">
                                     <div class="text-sm-right">
                                         <button type="button" class="btn btn-success mb-2 mr-1"><i class="mdi mdi-settings"></i></button>
@@ -36,36 +36,23 @@
                                     <table class="table table-centered table-striped" id="products-datatable">
                                         <thead>
                                             <tr>
-                                                {{-- <th style="width: 20px;">
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck1">
-                                                    <label class="custom-control-label" for="customCheck1">&nbsp;</label>
-                                                </div>
-                                            </th> --}}
-                                           
+
+
                                                 <th>Phone</th>
                                                 <th>Job Title</th>
                                                 <th>Expected Salary</th>
                                                 <th>Skill</th>
                                                 <th>Gender</th>
                                                 <th>Experience</th>
-                                              
+
 
                                                 {{-- <th>Status</th> --}}
-                                                
+
                                             </tr>
                                         </thead>
                                         <tbody>
                                             @foreach ($candidate as $applicant)
                                                 <tr>
-                                                    {{-- <td>
-                                                <div class="custom-control custom-checkbox">
-                                                    <input type="checkbox" class="custom-control-input" id="customCheck2">
-                                                    <label class="custom-control-label" for="customCheck2">&nbsp;</label>
-                                                </div>
-                                            </td> --}}
-
-                                                   
                                                     <td>
                                                         {{ $applicant->phone }}
                                                     </td>
@@ -78,23 +65,16 @@
                                                     <td>
                                                         {{ $applicant->skill }}
                                                     </td>
-                                                   
+
                                                     <td>
                                                         {{ $applicant->gender }}
                                                     </td>
                                                     <td>
                                                         {{ $applicant->job_experience }}
                                                     </td>
-                                                  
-                                                    {{-- <td>
-                                                        <span class="badge bg-soft-success text-success">Active</span>
-                                                    </td> --}}
-                                                   
-                 
+
                                                 </tr>
                                             @endforeach
-
-
 
                                         </tbody>
                                     </table>
@@ -129,10 +109,5 @@
             </div> <!-- container -->
 
         </div> <!-- content -->
-
-        <!-- Footer Start -->
-
-        <!-- end Footer -->
-
     </div>
 @endsection
