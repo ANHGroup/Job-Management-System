ebd19dd4e9130ce78fdcbf4367ea86cc81cf2351
mohib04 <mohib.agu@gmail.com>
2023-04-17 14:08:19 +0600

    logo link updated

--- resources/views/backend/layouts/topbar.blade.php
+++ resources/views/backend/layouts/topbar.blade.php
@@ -5,21 +5,21 @@
 
             <div class="card-body">
                 @if (auth()->check())
-                    <span>{{ auth()->user()->name ?? null }}</span>
+                <span>{{ auth()->user()->name ?? null }}</span>
                 @endif
-            {{-- <a href="" class="{{ route('applicant.edit',$profile->id) }}">Edit</a> --}}
+                {{-- <a href="" class="{{ route('applicant.edit',$profile->id) }}">Edit</a> --}}
             </div>
             <div class="card-body">
-               
+
             </div>
 
         </li>
- <li class="dropdown notification-list">
-            <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown"
-                href="" role="button" aria-haspopup="false" aria-expanded="false">
+        <li class="dropdown notification-list">
+            <a class="nav-link dropdown-toggle nav-user mr-0 waves-effect waves-light" data-toggle="dropdown" href=""
+                role="button" aria-haspopup="false" aria-expanded="false">
                 <img src="{{ asset('assets/images/logo/ANH_Group_logo_white.jpg') }}" alt="" class="rounded-circle">
                 <span class="pro-user-name ml-1">
-                    {{-- {{ Auth::user()->name }} <i class="mdi mdi-chevron-down"></i>  --}}
+                    {{-- {{ Auth::user()->name }} <i class="mdi mdi-chevron-down"></i> --}}
                 </span>
             </a>
             <div class="dropdown-menu dropdown-menu-right profile-dropdown ">
@@ -29,11 +29,11 @@
                 </div>
                 <div class="dropdown-divider"></div>
                 <a href="javascript:void(0);" class="dropdown-item notify-item">
-                   
+
                     <span><a href="">Edit</span>
-                   
+
                     <i class="fe-log-out"></i>
-                  
+
                 </a>
 
             </div>
@@ -42,7 +42,7 @@
 
     <!-- LOGO -->
     <div class="logo-box">
-        <a href="index.html" class="logo text-center">
+        <a href="/" class="logo text-center">
             <span class="logo-lg">
                 <img src="{{ asset('assets/images/logo/ANH_Group_logo_white.jpg') }}" alt="" height="60">
                 <!-- <span class="logo-lg-text-light">UBold</span> -->
@@ -61,22 +61,21 @@
             </button>
         </li>
         <li class="">
-            <a class="nav-link dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#"
-                role="button" aria-haspopup="false" aria-expanded="false">
+            <a class="nav-link dropdown-toggle waves-effect waves-light" data-toggle="dropdown" href="#" role="button"
+                aria-haspopup="false" aria-expanded="false">
                 @if (Route::has('login'))
-                    <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block topnav">
-                        @auth
-                            <a href="{{ url('/') }}"
-                                class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
-                        @else
-                            <a href="{{ route('login') }}" class="container-fluid">Log
-                                in</a>
+                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block topnav">
+                    @auth
+                    <a href="{{ url('/') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">Dashboard</a>
+                    @else
+                    <a href="{{ route('login') }}" class="container-fluid">Log
+                        in</a>
 
-                            @if (Route::has('register'))
-                                <a href="{{ route('register') }}" class="navbar-header">Register</a>
-                            @endif
-                        @endauth
-                    </div>
+                    @if (Route::has('register'))
+                    <a href="{{ route('register') }}" class="navbar-header">Register</a>
+                    @endif
+                    @endauth
+                </div>
                 @endif
             </a>
 
