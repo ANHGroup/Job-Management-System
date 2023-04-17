84905958224b08f56642feb5e32083ef83112463
tuhin cse <tuhincse20@gmail.com>
2023-04-17 09:54:38 +0600

    marge

--- app/Http/Controllers/AppliedJobController.php
+++ app/Http/Controllers/AppliedJobController.php
@@ -18,7 +18,7 @@ class AppliedJobController extends Controller
     public function index()
     {
         $user = Auth::user();
-        $jobs = DB::table('applied_jobs')//this is for user
+        $candidate = DB::table('applied_jobs') //this is for user
             ->join('applicant_profiles', 'applicant_profiles.id', '=', 'applied_jobs.applicant_id')
             ->join('jobs', 'jobs.id', '=', 'applied_jobs.job_id')
             ->where('user_id', $user->id)
