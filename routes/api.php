b6ae6def4b03c020d196a7df9edea7849c3ca8d0
tuhin cse <tuhincse20@gmail.com>
2023-04-18 10:27:20 +0600

    just add comment

--- routes/api.php
+++ routes/api.php
@@ -12,8 +12,8 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | is assigned the "api" middleware group. Enjoy building your API!
 |
-*/
-
+ */
+// it's for api.
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
