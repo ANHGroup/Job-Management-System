2031d72f712a4f0acadc533f7584290e5a3128c9
mohib04 <mohib.agu@gmail.com>
2023-05-21 18:28:25 +0600

    templating done

---
+++ resources/views/frontend/partials/footer.blade.php
@@ -0,0 +1,35 @@
+<!-- Footer Start -->
+<footer class="footer">
+  <div class="container-fluid">
+    <div class="row">
+      <div class="col-md-6 text-center">
+        2023 &copy; All rights reserved <a href="https://www.anhgroupbd.com/">ANH GROUP</a>
+      </div>
+    </div>
+</footer>
+<!-- end Footer -->
+
+
+<!-- Vendor js -->
+<script src="assets/js/vendor.min.js"></script>
+
+<!-- Plugins js-->
+<script src="assets/libs/flatpickr/flatpickr.min.js"></script>
+<script src="assets/libs/jquery-knob/jquery.knob.min.js"></script>
+<script src="assets/libs/jquery-sparkline/jquery.sparkline.min.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.resize.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.time.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.tooltip.min.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.selection.js"></script>
+<script src="assets/libs/flot-charts/jquery.flot.crosshair.js"></script>
+
+<!-- Dashboar 1 init js-->
+<script src="assets/js/pages/dashboard-1.init.js"></script>
+
+<!-- App js-->
+<script src="assets/js/app.min.js"></script>
+
+</body>
+
+</html>
