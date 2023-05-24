64def336e2bb5b2334bda0d9d17a17cd1a79606e
mohib04 <mohib.agu@gmail.com>
2023-05-24 11:40:06 +0600

    Admin login & registration controller and view added

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Admin\AdminDashboardController;
+use App\Http\Controllers\Admin\AminLoginController;
+use App\Http\Controllers\Admin\AminRegistrationController;
+
 use App\Http\Controllers\User\UserDashboardController;
 
 //Home route
@@ -10,17 +13,23 @@ Route::get('/', function () {
     return view('users.pages.home')->with('user', auth()->user());
 });
 
-// Normal user dashboard route
+// Normal user route
 Route::middleware(['auth', 'verified'])->prefix('user')->group(function () {
     Route::get('dashboard', [UserDashboardController::class, 'index'])->name('user.dashboard');
 });
 
 
-// Admin dashboard route
-Route::get('admin/dashboard', [AdminDashboardController::class, 'index']);
+// Admin route
+Route::prefix('admin')->group(function () {
+    Route::get('login', [AminLoginController::class, 'index']);
+    Route::post('login', [AminLoginController::class, 'create']);
+    Route::get('registration', [AminRegistrationController::class, 'index']);
+    Route::post('registration', [AminRegistrationController::class, 'store']);
 
+    Route::get('dashboard', [AdminDashboardController::class, 'index']);
+});
 
-//User Profiles
+//Normal User Profiles
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'index'])->name('profile');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
