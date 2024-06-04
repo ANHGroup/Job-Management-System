78684e5d578053ea6e058f5e38ac2850c1cc8f5b
mohib04 <mohib.agu@gmail.com>
2024-06-04 16:33:20 +0600

    fix cors origin problem and forntend api call issue

--- frontend/src/components/Dashboard.vue
+++ frontend/src/components/Dashboard.vue
@@ -80,14 +80,33 @@
 
     <header class="bg-white shadow">
       <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-        <h1 class="text-3xl font-bold tracking-tight text-gray-900">Dashboard</h1>
+        <h1 class="text-3xl font-bold tracking-tight text-gray-900">All jobs</h1>
       </div>
     </header>
     <main>
-      <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-        <!-- Your content -->
+    <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+      <div v-for="job in jobs" :key="job.id" class="w-full bg-white shadow-lg rounded-lg overflow-hidden mb-6">
+        <div class="px-6 py-4">
+          <h2 class="text-xl font-semibold text-gray-900">{{ job.title }}</h2>
+          <p class="text-gray-600 mt-2">{{ job.description }}</p>
+        </div>
+        <div class="px-6 py-2 flex items-center">
+          <div class="mr-4 flex items-center">
+            <i class="fas fa-building pr-2"></i>
+            <p class="text-gray-600">{{ job.company }}</p>
+          </div>
+          <div class="mr-4 flex items-center">
+            <i class="fas fa-location-pin pr-2"></i>
+            <p class="text-gray-600">{{ job.location }}</p>
+          </div>
+          <div class="mr-4 flex items-center">
+            <i class="fas fa-money-bill-wave pr-2"></i>
+            <p class="text-gray-600">{{ job.salary }} TK</p>
+          </div>
+        </div>
       </div>
-    </main>
+    </div>
+  </main>
   </div>
 </template>
 
@@ -113,4 +132,22 @@ const userNavigation = [
   { name: 'Settings', href: '#' },
   { name: 'Sign out', href: '#' },
 ]
+
+
+import axios from 'axios';
+import { ref, onMounted } from 'vue';
+
+// Create reactive variables
+const jobs = ref([]);
+
+// Fetch data on component mount
+onMounted(async () => {
+  try {
+    // Fetch data from the jobs API
+    const jobsResponse = await axios.get("http://127.0.0.1:8000/api/jms_jobs");
+    jobs.value = jobsResponse.data;
+  } catch (error) {
+    console.error("Failed to fetch data", error);
+  }
+});
 </script>
